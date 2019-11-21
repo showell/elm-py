6f708bc4c329f4bfc44d8bb49da60560882caf3c
Steve Howell <showell30@yahoo.com>
2019-11-21 07:57:13 -0500

    Add ranks example

---
+++ ranks.py
@@ -0,0 +1,28 @@
+from Elm import F, wrap, pipe
+from Kernel import toElm, toPy
+import List
+import Tuple
+
+# We could do this F-wrapping more automatically, but
+# here we do it explicitly.
+List.indexedMap = F(List.indexedMap)
+List.sortBy = F(List.sortBy)
+List.map_ = F(List.map_)
+Tuple.pair = F(Tuple.pair)
+Tuple.first = F(Tuple.first)
+Tuple.second = F(Tuple.second)
+
+@wrap(toElm, toPy)
+def ranks(lst):
+    return pipe(
+            lst, [
+                (List.indexedMap) (Tuple.pair),
+                (List.sortBy) (Tuple.second),
+                (List.map_) (Tuple.first),
+                (List.indexedMap) (Tuple.pair),
+                (List.sortBy) (Tuple.second),
+                (List.map_) (Tuple.first),
+            ])
+
+print(ranks([77, 0, 66, 22, 55, 99, 44, 33, 11, 88]))
+
