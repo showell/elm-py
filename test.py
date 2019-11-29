9b76561bdc88d6c5f32af5e7d82a0c80d9c6e480
Steve Howell <showell30@yahoo.com>
2019-11-29 16:26:03 -0500

    Extract testHelper.py

--- test.py
+++ test.py
@@ -14,61 +14,20 @@ import Maybe
 import Order
 import Tuple
 
+from testHelper import (
+        printList,
+        assertTrue,
+        assertFalse,
+        assertFloat,
+        assertEqual,
+        assertList,
+        )
+
 isList = ListKernel.isList
 
 F = Elm.F
 Nothing = Maybe.Nothing
 Just = Maybe.Just
-
-def printList(xs):
-    print(toPy(xs))
-
-def assertTrue(actual):
-    if actual == True:
-        print('pass')
-    else:
-        print('\n\nFAIL!\n', actual)
-        print('\n')
-        raise AssertionError
-
-def assertFalse(actual):
-    if actual == False:
-        print('pass')
-    else:
-        print('\n\nFAIL!\n', actual)
-        print('\n')
-        raise AssertionError
-
-def assertFloat(actual, expected):
-    if type(actual) == tuple:
-        assert len(actual) == 2
-        assert len(expected) == 2
-        assertFloat(actual[0], expected[0])
-        assertFloat(actual[1], expected[1])
-        return
-
-    if abs(actual - expected) < 0.000001:
-        print('pass')
-    else:
-        print('\n\nFAIL!\n', actual, expected)
-        print('\n')
-        raise AssertionError
-
-def assertEqual(actual, expected):
-    if actual == expected:
-        print('pass')
-    else:
-        print('\n\nFAIL!\n', actual, expected)
-        print('\n')
-        raise AssertionError
-
-def assertList(elmList, expected):
-    if toPy(elmList) == expected:
-        print('pass')
-    else:
-        print('\n\nFAIL!\n', elmList, expected)
-        print('\n')
-        raise AssertionError
 
 even = lambda x : x % 2 == 0
 negative = lambda x : x < 0
