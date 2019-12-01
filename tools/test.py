4575a7380afef0f47c9e9e21930e3daa6ef7f883
Steve Howell <showell30@yahoo.com>
2019-12-01 06:28:56 -0500

    clean up tests

--- tools/test.py
+++ tools/test.py
@@ -25,238 +25,240 @@ def succeed(fCapture, s):
 def assertIndent(s, i, expected):
     assert parse.indentLevel(s, i) == expected
 
-assertIndent('', 0, 0)
-assertIndent('\n', 0, 0)
-assertIndent('\n', 1, 0)
-assertIndent('  fred', 0, 2)
-assertIndent(' \n  fred', 3, 2)
-assertIndent(' \n  fred', 4, 2)
-assertIndent(' \n  fred', 5, 2)
+def testIndents():
+    assertIndent('', 0, 0)
+    assertIndent('\n', 0, 0)
+    assertIndent('\n', 1, 0)
+    assertIndent('  fred', 0, 2)
+    assertIndent(' \n  fred', 3, 2)
+    assertIndent(' \n  fred', 4, 2)
+    assertIndent(' \n  fred', 5, 2)
 
-succeed(parseElm.captureModule,
-    """
-    module foo exposing (
+def testParse():
+    succeed(parseElm.captureModule,
+        """
+        module foo exposing (
+            foo, bar
+            )
+        """)
+
+    succeed(parseElm.captureDocs,
+        """
+        {-|
+           bla bla bla
+        -}
+        """)
+
+    succeed(parseElm.captureImport,
+        """
+    import foo exposing ,
         foo, bar
         )
-    """)
-
-succeed(parseElm.captureDocs,
-    """
-    {-|
-       bla bla bla
-    -}
-    """)
-
-succeed(parseElm.captureImport,
-    """
-import foo exposing ,
-    foo, bar
-    )
-    """)
-
-succeed(parseElm.captureType,
-    """
-    type Value =
-        = Int
-        | String
-    """)
-
-succeed(parseElm.captureDef,
-    """
-    x =
-    """)
-
-succeed(parseElm.captureBinding,
-    """
-    x =
-        5""")
-
-succeed(parseElm.captureExpr,
-    """
-    foo bar
         """)
 
-succeed(parseElm.captureLet,
-    """
-    let
-        foo a b c =
-            one
+    succeed(parseElm.captureType,
+        """
+        type Value =
+            = Int
+            | String
+        """)
 
-        bar x y z =
-            two
-    in
-    foo bar""")
+    succeed(parseElm.captureDef,
+        """
+        x =
+        """)
 
-succeed(parseElm.captureOneCase,
-    """
-    foo ->
+    succeed(parseElm.captureBinding,
+        """
+        x =
+            5""")
+
+    succeed(parseElm.captureExpr,
+        """
+        foo bar
+            """)
+
+    succeed(parseElm.captureLet,
+        """
         let
-            x =
-                2
+            foo a b c =
+                one
+
+            bar x y z =
+                two
         in
-        x
+        foo bar""")
 
-        """)
-
-succeed(parseElm.captureCaseOf,
-    """
-    case fred of
-        """)
-
-succeed(parseElm.captureCase,
-    """
-    case fred of
+    succeed(parseElm.captureOneCase,
+        """
         foo ->
-            f foo
-                bla
+            let
+                x =
+                    2
+            in
+            x
 
-        bar ->
-            f bar
-                bla
-        """)
+            """)
 
-succeed(parseElm.captureIf,
-    """
-    if cond then
-        if cond2 then
-            a
+    succeed(parseElm.captureCaseOf,
+        """
+        case fred of
+            """)
+
+    succeed(parseElm.captureCase,
+        """
+        case fred of
+            foo ->
+                f foo
+                    bla
+
+            bar ->
+                f bar
+                    bla
+            """)
+
+    succeed(parseElm.captureIf,
+        """
+        if cond then
+            if cond2 then
+                a
+            else
+                b
+
         else
-            b
+            false_val
+                stuff
+            """)
 
-    else
-        false_val
-            stuff
-        """)
+    # tuples are dumb now
+    succeed(parseElm.captureExprTuple,
+        """
+            ( foo, bar )
+            """)
 
-# tuples are dumb now
-succeed(parseElm.captureExprTuple,
-    """
-        ( foo, bar )
-        """)
+    succeed(parseElm.captureExpr,
+        """
+            add 5 7
+            """)
 
-succeed(parseElm.captureExpr,
-    """
-        add 5 7
-        """)
+    succeed(parseElm.captureAnnotation,
+        """
+    foo : List String  ->
+       String ->
+       Int""")
 
-succeed(parseElm.captureAnnotation,
-    """
-foo : List String  ->
-   String ->
-   Int""")
+    succeed(parseElm.captureUnit, '()')
+    succeed(parseElm.captureUnit, '(  )')
 
-succeed(parseElm.captureUnit, '()')
-succeed(parseElm.captureUnit, '(  )')
-
-succeed(parseElm.captureLambda,
-    """
-    \\() -> a (b c)
-    """)
-
-
-succeed(parseElm.captureLambda,
-    """
-    \\a b -> c
-    """)
-
-
-succeed(parseElm.captureLambda,
-    """
-    \\x y ->
-        if b then
-            t
-        else
-            f
-    """)
-
-# tuples in lambda params
-succeed(parseElm.captureExpr,
-    """
-    \\(x,y) -> a
-    """)
-
-
-# lambda in expressions
-succeed(parseElm.captureExpr,
-    """
-    map2 ( \\x y -> 5 ) lst
-    """)
-
-
-succeed(parseElm.capturePatternList,
-    """
-    [ a, 5, c ]
-    """)
-
-
-succeed(parseElm.capturePatternDef,
-    """
-    [x, y] ->
-    """)
-
-succeed(parseElm.capturePatternExpr,
-    """
-    []
-    """)
-
-succeed(parseElm.captureOneCase,
-    """
-    foo bar ->
-        5
-        """)
-
-succeed(parseElm.captureOneCase,
-    """
-    foo (x y) ->
-        5
-        """)
-
-succeed(parseElm.captureExpr,
-    """
-    x < y
-        """)
-
-succeed(parseElm.captureOneCase,
-    """
-    head :: rest ->
-        5
+    succeed(parseElm.captureLambda,
+        """
+        \\() -> a (b c)
         """)
 
 
-succeed(parseElm.captureOneCase,
-    """
-    (first, second) :: rest ->
-        5
-        """)
-
-succeed(parseElm.captureIf,
-    """
-    if foo then 1 else 2
+    succeed(parseElm.captureLambda,
+        """
+        \\a b -> c
         """)
 
 
-succeed(parseElm.captureExpr,
-    """
-    n+1
+    succeed(parseElm.captureLambda,
+        """
+        \\x y ->
+            if b then
+                t
+            else
+                f
+        """)
+
+    # tuples in lambda params
+    succeed(parseElm.captureExpr,
+        """
+        \\(x,y) -> a
         """)
 
 
-succeed(parseElm.captureTupleVar,
-    """
-    (x, y)
-        """)
-
-succeed(parseElm.captureBinding,
-    """
-    (x, y) =
-        foo
+    # lambda in expressions
+    succeed(parseElm.captureExpr,
+        """
+        map2 ( \\x y -> 5 ) lst
         """)
 
 
-succeed(parseElm.captureCall,
-    """
-    foo (bar x) y
+    succeed(parseElm.capturePatternList,
+        """
+        [ a, 5, c ]
         """)
+
+
+    succeed(parseElm.capturePatternDef,
+        """
+        [x, y] ->
+        """)
+
+    succeed(parseElm.capturePatternExpr,
+        """
+        []
+        """)
+
+    succeed(parseElm.captureOneCase,
+        """
+        foo bar ->
+            5
+            """)
+
+    succeed(parseElm.captureOneCase,
+        """
+        foo (x y) ->
+            5
+            """)
+
+    succeed(parseElm.captureExpr,
+        """
+        x < y
+            """)
+
+    succeed(parseElm.captureOneCase,
+        """
+        head :: rest ->
+            5
+            """)
+
+
+    succeed(parseElm.captureOneCase,
+        """
+        (first, second) :: rest ->
+            5
+            """)
+
+    succeed(parseElm.captureIf,
+        """
+        if foo then 1 else 2
+            """)
+
+
+    succeed(parseElm.captureExpr,
+        """
+        n+1
+            """)
+
+
+    succeed(parseElm.captureTupleVar,
+        """
+        (x, y)
+            """)
+
+    succeed(parseElm.captureBinding,
+        """
+        (x, y) =
+            foo
+            """)
+
+
+    succeed(parseElm.captureCall,
+        """
+        foo (bar x) y
+            """)
 
 
 def testBlocks():
@@ -316,4 +318,6 @@ def testBlocks():
         parse.parseBlock(state),
         'in foo')
 
+testIndents()
+testParse()
 testBlocks()
