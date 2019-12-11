62b39c371cc9abc1c54be6b625af4fe803cd42b0
Steve Howell <showell30@yahoo.com>
2019-12-11 07:00:31 -0500

    wip pratt

--- parser/tests/testPratt.py
+++ parser/tests/testPratt.py
@@ -20,14 +20,17 @@ class Var:
         return self.token
 
 class VarToken:
+    lbp = 0
+
     def __init__(self, token):
         self.token = token
 
     def __str__(self):
         return self.token
 
-    def nud(self, state):
-        return (Var(self.token), state)
+    def nud(self, token, state):
+        self.ast = Var(self.token)
+        return (self, token, state)
 
 class Op:
     def __init__(self, first, op, second):
@@ -41,33 +44,43 @@ class Op:
 class OpToken:
     def __init__(self, token):
         self.token = token
+        if token == '+':
+            self.lbp = 60
+        elif token == '*':
+            self.lbp = 70
 
     def __str__(self):
-        return self.token
+        return str(self.ast)
 
-    def led(self, state, left):
-        res = tokenize(state)
-        token = res.ast
-        (right, state) = expression(token, state)
-        ast = Op(left, self.token, right)
-        return (ast, state)
+    def led(self, left, token, state):
+        (right, token, state) = expression(token, state, self.lbp)
+        self.ast = Op(left, self.token, right)
+        return (self, token, state)
 
-def expression(token, state):
+def expression(token, state, rbp=0):
+    # This code is adapted from this article:
+    #
+    # http://effbot.org/zone/simple-top-down-parsing.htm
     t = token
     res = tokenize(state)
+    if res is None:
+        return t.nud(token, state)
+
     token = res.ast
     state = res.state
-    (left, state) = t.nud(state)
+    (left, token, state) = t.nud(token, state)
 
-    while True:
+    while rbp < token.lbp:
         t = token
         res = tokenize(state)
         if res is None:
+            print('break')
             break
-        (left, state) = t.led(state, left)
-        break
+        token = res.ast
+        state = res.state
+        (left, token, state) = t.led(left, token, state)
 
-    return (left, state)
+    return (left, token, state)
 
 
 var = \
@@ -79,22 +92,21 @@ var = \
 op = \
     transform(
         OpToken,
-        captureOperator(['<']),
+        captureOperator(['<', '+', '*']),
     )
 
-def tokenize(state):
-    res = captureOneOf(
+tokenize = \
+    captureOneOf(
         var,
-        op
-    )(state)
-    return res
+        op,
+        )
 
 def testTokens():
-    s = "x < y"
+    s = "a * b + c * d + e"
     state = State(s)
 
     res = tokenize(state)
-    (ast, state) = expression(res.ast, res.state)
-    print('final ast', str(ast))
+    (ast, token, state) = expression(res.ast, res.state)
+    print('final ast', str(ast.ast))
 
 testTokens()
